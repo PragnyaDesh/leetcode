3e470f58a10ae72936d296dcf0f3cc4e51faf81b
PragnyaDesh <60569711+PragnyaDesh@users.noreply.github.com>
2021-01-04 19:44:45 +0530

    #9

---
+++ CPPproblems/CPPproblems/PalindromeNumber.hpp
@@ -0,0 +1,16 @@
+//
+//  PalindromeNumber.hpp
+//  CPPproblems
+//
+//  Created by Pragnya Deshpande on 04/01/21.
+//
+
+#ifndef PalindromeNumber_hpp
+#define PalindromeNumber_hpp
+
+#include <stdio.h>
+
+using namespace std;
+
+boolean isPalindrome(int x);
+#endif /* PalindromeNumber_hpp */
