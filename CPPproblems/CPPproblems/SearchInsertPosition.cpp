f5778f848edff4851d141d85ceecb8827d51620b
PragnyaDesh <60569711+PragnyaDesh@users.noreply.github.com>
2021-01-07 22:42:56 +0530

    #35

---
+++ CPPproblems/CPPproblems/SearchInsertPosition.cpp
@@ -0,0 +1,23 @@
+//
+//  SearchInsertPosition.cpp
+//  CPPproblems
+//
+//  Created by Pragnya Deshpande on 07/01/21.
+//
+
+#include "SearchInsertPosition.hpp"
+
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+       if(target<nums[0])
+           return 0;
+       for(int i=0; i<nums.size(); i++){
+           if(nums[i]>=target)
+               return i;
+           else if(i==nums.size()-1)
+               return nums.size();
+       }
+        return 0;
+    }
+};
---
+++ CPPproblems/CPPproblems/SearchInsertPosition.hpp
@@ -0,0 +1,16 @@
+//
+//  SearchInsertPosition.hpp
+//  CPPproblems
+//
+//  Created by Pragnya Deshpande on 07/01/21.
+//
+
+#ifndef SearchInsertPosition_hpp
+#define SearchInsertPosition_hpp
+
+#include <stdio.h>
+#include <vector>
+using namespace std;
+
+int searchInsert(vector<int>& nums, int target);
+#endif /* SearchInsertPosition_hpp */
